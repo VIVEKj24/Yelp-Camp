cc09a5ae571b2cf6434c1cb1d6dd644e71c4d52a
VIVEKj24 <vjvivekjain2406@gmail.com>
2025-01-15 01:59:00 +0530

    port

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.use((err,req,res,next)=>{
     res.status()
     res.status(statusCode).render('error',{err})
 })
-
-app.listen(3000,()=>{
-    console.log("listening on port 3000")
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
 })
